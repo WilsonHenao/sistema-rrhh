1f49334d6209b1498d458f0b295ad3fe8bd44c85
Wilson Henao <wilson.henao@utp.edu.co>
2023-05-18 12:13:44 -0500

    Se agrega ruta y controlador de getUsuarios

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -5,9 +5,17 @@ export const createUsuario = async (req, res) => {
   const {id, nombre, cargo, usuario, password} = req.body
   const pass = bcrypt.hashSync(password, 10)
   pool.query('INSERT INTO usuarios (id, nombre, cargo, usuario, contraseña) VALUES (?,?,?,?,?)', [id, nombre, cargo, usuario, pass], (error, results) => {
-    if (error) throw error;
+    if (error)
+      throw error;
     res.send({
       id, nombre, cargo, usuario, pass
     });
   });
+}
+
+export const getUsuarios = async (req, res) => {
+  pool.query('SELECT * FROM usuarios', (error, results) => {
+    if (error) throw error;
+    res.send(results)
+  })
 }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createUsuario } from "../controllers/usuarios.controller.js";
+import { createUsuario, getUsuarios } from "../controllers/usuarios.controller.js";
 
 const router = Router()
 
 router.post('/usuarios', createUsuario)
+router.get('/usuarios', getUsuarios)
 
 export default router
