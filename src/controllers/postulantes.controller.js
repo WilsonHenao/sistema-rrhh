de8446fd440281b1c9ff505007e47c998cc71f25
Wilson Henao <wilson.henao@utp.edu.co>
2023-06-11 15:55:11 -0500

    Correccion

--- src/controllers/postulantes.controller.js
+++ src/controllers/postulantes.controller.js
@@ -4,6 +4,9 @@ export const createPostulante = async(req, res) => {
   const {id, nombre, email, telefono, idVacante} = req.body
   const estado = "Postulado"
   console.log(req.body)
+  res.send({
+    id, nombre, email, telefono, estado, idVacante
+  })
   /*
   pool.query('INSERT INTO postulantes (id, nombre, email, telefono, estado, idVacante) VALUES (?, ?, ?, ?, ?, ?)', [id, nombre, email, telefono, estado, idVacante], (error, results) => {
     if (error) throw error;
