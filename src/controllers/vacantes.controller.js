53e75a2049f344835ebe8490b12f41686408e99b
Wilson Henao <wilson.henao@utp.edu.co>
2023-06-14 01:14:00 -0500

    Se agrega funcionalidad editar vacante

--- src/controllers/vacantes.controller.js
+++ src/controllers/vacantes.controller.js
@@ -28,3 +28,15 @@ export const createCargo = async(req, res) => {
     });
   });
 }
+
+export const updateVacante = async (req, res) => {
+  const {id} = req.params
+  const {nombre, descripcion, idCargo, idUsuario} = req.body
+
+  pool.query('UPDATE usuarios SET nombre = ?, descripcion = ?, idCargo = ?, idUsuario = ? WHERE id = ?', [nombre, descripcion, idCargo, idUsuario, id], (error, results) => {
+    if (error) throw error;
+    res.send({
+      id, nombre, descripcion, idCargo, idUsuario
+    });
+  });
+}
--- src/routes/vacantes.routes.js
+++ src/routes/vacantes.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createCargo, createVacante, getVacantes } from "../controllers/vacantes.controller.js";
+import { createCargo, createVacante, getVacantes, updateVacante } from "../controllers/vacantes.controller.js";
+import { authenticateToken } from "../controllers/usuarios.controller.js";
 
 const router = Router()
 
 router.get('/vacantes', getVacantes)
 router.post('/vacantes', createVacante)
 router.post('/cargo', createCargo)
+router.put('/vacantes/:id', authenticateToken, updateVacante)
 
 export default router
